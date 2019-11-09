5bea67477bf31ebbee9965d7210499cbd9d93898
teriyakiegg <teriyakiegg@gmail.com>
2019-11-10 00:36:32 +0900

    Revert "For tutorial mod"
    This reverts commit 86212fb9c27e9ceb2949d312e4434fcfbb759cf7.

--- main.go
+++ main.go
@@ -7,7 +7,6 @@ import (
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/heroku/x/hmetrics/onload"
-        "github.com/russross/blackfriday"
 )
 
 func main() {
@@ -24,10 +23,6 @@ func main() {
 
 	router.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.tmpl.html", nil)
-	})
-
-        router.GET("/mark", func(c *gin.Context) {
-		c.String(http.StatusOK, string(blackfriday.Run([]byte("**hi!**"))))
 	})
 
 	router.Run(":" + port)
